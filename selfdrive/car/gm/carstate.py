7e116a2c125e732fa66e07514a4c8ba6c53ef225
Bolt5627 <115066584+bolt5627@users.noreply.github.com>
2023-02-04 20:54:00 +0900

    Update carstate.py

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -46,7 +46,7 @@ class CarState(CarStateBase):
     ret.vEgoRaw = mean([ret.wheelSpeeds.fl, ret.wheelSpeeds.fr, ret.wheelSpeeds.rl, ret.wheelSpeeds.rr])
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
     # sample rear wheel speeds, standstill=True if ECM allows engagement with brake
-    ret.vEgoCluster = ret.vEgo * 1.0675
+    ###ret.vEgoCluster = ret.vEgo * 1.0675
     ret.standstill = ret.wheelSpeeds.rl <= STANDSTILL_THRESHOLD and ret.wheelSpeeds.rr <= STANDSTILL_THRESHOLD
 
     if pt_cp.vl["ECMPRDNL2"]["ManualMode"] == 1:
